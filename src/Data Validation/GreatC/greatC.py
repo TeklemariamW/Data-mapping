05d7c712329f61eedcb63600c1a815b493650b9b
Weldehawariat <tweldehawariat@deloitte.com>
2025-01-09 14:53:57 -0500

    implement great expectation on greatC.py file

--- src/Data Validation/GreatC/greatC.py
+++ src/Data Validation/GreatC/greatC.py
@@ -1,13 +1,17 @@
 # pip install great_expectations
+import os
+import pandas as pd
 import great_expectations as ge
+from great_expectations.dataset.pandas_dataset import PandasDataset
 
 # Load the data
-df = ge.read_csv("customer_data.csv")
-
+# df = ge.read_csv("customer_data.csv")
+df = pd.read_csv("tmp/combined_data.csv")
+df = PandasDataset(df)
 # Create a new expectation suite
-df.expect_column_to_exist("customer_id")
-df.expect_column_values_to_not_be_null("customer_id")
-df.expect_column_values_to_be_unique("customer_id")
+df.expect_column_to_exist("combined_id")
+df.expect_column_values_to_not_be_null("id")
+df.expect_column_values_to_be_unique("id")
 
 df.expect_column_to_exist("email")
 df.expect_column_values_to_match_regex("email", r"[^@]+@[^@]+\.[^@]+")
@@ -15,5 +19,12 @@ df.expect_column_values_to_match_regex("email", r"[^@]+@[^@]+\.[^@]+")
 df.expect_column_to_exist("age")
 df.expect_column_values_to_be_between("age", 18, 99)
 
+
+# Ensure the directory exists
+expectations_dir = "expectations"
+if not os.path.exists(expectations_dir):
+    os.makedirs(expectations_dir)
+
 # Save the expectation suite
-df.save_expectation_suite("expectations/customer_data_expectations.json")
+df.save_expectation_suite(expectations_dir, "customer_data_expectations.json")
+print(df.validate())
